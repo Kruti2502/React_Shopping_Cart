6e8d8c8c24a7c3f90263361bc9d791e651dd5f24
Kruti2502 <kruti.shah1625@gmail.com>
2022-11-27 22:48:47 +0530

    Updated route config

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
       <Navbar />
       <Container className="mb-4">
         <Routes>
-          <Route path="/store" element={<Store />} />
+          <Route path="/" element={<Store />} />
         </Routes>
       </Container>
     </ShoppingCartProvider>
